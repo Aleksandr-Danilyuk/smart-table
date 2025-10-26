9c1cc0cd72947c9dbf5a37edf100b31422a20750
Alexander Danilyuk <danilyuk25@yandex.ru>
2025-10-27 01:42:55 +0300

    ТЗ 2. Пагинация через сервер

--- src/main.js
+++ src/main.js
@@ -37,6 +37,22 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
+
+async function render(action) {
+    let state = collectState(); // состояние полей из таблицы
+    let query = {}; // здесь будут формироваться параметры запроса
+    // другие apply*
+    // result = applySearching(result, state, action);
+    // result = applyFiltering(result, state, action);
+    // result = applySorting(result, state, action);
+    query = applyPagination(query, state, action); // обновляем query
+
+    const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
+
+    updatePagination(total, query); // перерисовываем пагинатор
+    sampleTable.render(items);
+}
+/*
 async function render(action) {
     let state = collectState(); // состояние полей из таблицы
     //let result = [...data]; // копируем для последующего изменения
@@ -51,6 +67,7 @@ async function render(action) {
     //sampleTable.render(result)
     sampleTable.render(items)
 }
+*/
 
 const sampleTable = initTable({
     tableTemplate: 'table',
@@ -71,7 +88,8 @@ const applySorting = initSorting([        // Нам нужно передать 
 //    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
 //});
 
-const applyPagination = initPagination(
+//const applyPagination = initPagination(
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
     (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
         const input = el.querySelector('input');
